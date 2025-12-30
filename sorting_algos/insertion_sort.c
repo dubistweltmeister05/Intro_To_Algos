cfb8a8c528df64d7c3379097029c00fbfced0478
dubistweltmeister05 <kshitijvaze@gmail.com>
2025-12-30 08:39:10 +0530

    Insertion sort algo

---
+++ sorting_algos/insertion_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void print_arr(int *arr, int size)
+{
+    printf("\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("\t%d\n", arr[i]);
+    }
+}
+int main()
+{
+    int arr[6] = {5, 2, 4, 6, 1, 3};
+
+    print_arr(arr, 6);
+    for (int j = 1; j < 6; j++)
+    {
+        int key = arr[j];
+        int i = j - 1;
+
+        while (i >= 0 && arr[i] > key)
+        {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+        arr[i + 1] = key;
+    }
+
+    print_arr(arr, 6);
+
+    return 0;
+}
